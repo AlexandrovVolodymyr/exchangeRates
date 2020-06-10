8be3b52c313230bcebe608ec9d7b19f18fff61df
Alexandrov Vladimir <dev.alexandrovhub@gmail.com>
2020-06-10 13:58:37 +0300

    change server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/exhcange-rates'));
+app.use(express.static(__dirname + '/dist/exhcangeRates'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/exhcange-rates/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/exhcangeRates/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
